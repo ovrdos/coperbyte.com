6ce043c3182436b15529df67756b585886c2fe7b
Kamal Hakim <coperbyte.sol@gmail.com>
2024-12-07 19:45:18 -0700

    clearing deploy paths

---
+++ coperbyte/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://coperbyte.com" target="_blank">
+         C O P E R B Y T E   S O L U T I O N S  
+        </a>
+      </div>
+    </>
+  )
+}
+
+export default App
